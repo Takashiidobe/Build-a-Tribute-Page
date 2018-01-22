fca7282662b6e340b6093e4e4862a8a50060886d
Takashiidobe <idobetakashi@gmail.com>
2018-01-21 20:32:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var myTitle = document.querySelector('title');
-myTitle.textContent = 'Dr. Norman Bourlag'
+myTitle.textContent = 'Dr. Norman Borlaug'
 
 $(document).ready(function() {
-    
+    $( "#toggle" ).toggle( "bounce", { times: 3 }, "slow" );
 });
